bb794478312a07421417b9646244bbf92eb89487
ahren <540464544@qq.com>
2019-06-06 13:52:30 +0800

    【BUG】推荐采集人员页面无法展示

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -29,16 +29,20 @@ var size = {
  * @param url
  */
 function parseURL(url){
-    var url = url.split("?")[1];
-    var para = url.split("&");
-    var len = para.length;
-    var res = {};
-    var arr = [];
-    for(var i=0;i<len;i++){
-        arr = para[i].split("=");
-        res[arr[0]] = arr[1];
+    if(url.indexOf("?") != -1){
+        var url = url.split("?")[1];
+        var para = url.split("&");
+        var len = para.length;
+        var res = {};
+        var arr = [];
+        for(var i=0;i<len;i++){
+            arr = para[i].split("=");
+            res[arr[0]] = arr[1];
+        }
+        return res;
+    }else{
+        return null;
     }
-    return res;
 }
 
 function fixWidthWithOptWidth(percent) {
